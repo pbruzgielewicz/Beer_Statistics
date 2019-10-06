3122a7ae1aa77077edc25e1a3d38e620f81f5f7e
pbruzgielewicz <p.bruzgielewicz@gmail.com>
2019-10-06 20:34:26 +0200

    pom update

--- src/main/java/com/beer_statistics/demo/DemoApplication.java
+++ src/main/java/com/beer_statistics/demo/DemoApplication.java
@@ -2,12 +2,25 @@ package com.beer_statistics.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleContextResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean(name = "localeResolver")
+    public LocaleContextResolver getLocaleContextResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(new Locale("pl", "PL"));
+        return localeResolver;
     }
 
 }
---
+++ src/main/java/com/beer_statistics/demo/model/Beer.java
@@ -0,0 +1,5 @@
+package com.beer_statistics.demo.model;
+
+
+public class Beer {
+}
